0a5a738a271651eff5de4fcd41e80ab4001b8019
David Martinez Marti <deavidsedice@gmail.com>
2011-01-09 13:31:49 +0100

    Release Version 0.1.1

--- bjsonrpc/__init__.py
+++ bjsonrpc/__init__.py
@@ -8,7 +8,7 @@
 """
 
 __version__ = '0.1'
-__release__ = '0.1.x-dev'
+__release__ = '0.1.1'
 
 
 __all__ = [
--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ from distutils.core import setup
 
 setup(
     name = 'bjsonrpc',
-    version = '0.1.x-dev',
+    version = '0.1.1',
     description = 'Asynchronous Bidirectional JSON-RPC protocol implementation over TCP/IP',
     author = 'David Martinez Marti',
     author_email = 'deavidsedice@gmail.com',
